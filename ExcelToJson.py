5d10f7611744aca2621c7a7befa299e17b6e27e4
susananson <46920166+susananson@users.noreply.github.com>
2019-01-22 18:16:18 +0530

    Add files via upload

--- ExcelToJson.py
+++ ExcelToJson.py
@@ -6,17 +6,28 @@ wb = xlrd.open_workbook("D:ExcelPoc.xlsx")
 #wb = xlrd.open_workbook('ExcelPoc.xls')
 sh = wb.sheet_by_index(0)
 # List to hold dictionaries
-cars_list = []
+
+emyList={}
+def lower_dict(d):
+   new_dict = dict((k.lower(), v) for k, v in d.items())
+   return new_dict
 # Iterate through each row in worksheet and fetch values into dict
 for rownum in range(1, sh.nrows):
-    cars = OrderedDict()
+   
     row_values = sh.row_values(rownum)
-    cars[row_values[0]] = row_values[1]
+
+    emyList.setdefault(row_values[0], []).append(row_values[1])
+    #cars[row_values[0]] = row_values[1]
    
    
-    cars_list.append(cars)
+    
 # Serialize the list of dicts to JSON
-j = json.dumps(cars_list)
+
+
+
+
+
+s=json.dumps(lower_dict({x.translate({32: None}): y for x, y in emyList.items()}))
 # Write to file
 with open('D:\\ExcelToJson.txt', 'w') as f:
-    f.write(j)
+    f.write(s)
